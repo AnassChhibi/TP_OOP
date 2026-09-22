cae4f6da76f44f468471377b3030a5c29219ee2a
Chhibi Anass <guillaumedelanoie01@gmail.com>
2026-09-22 17:39:53 +0200

    Exercice 8 fini

--- tp2/personnes.py
+++ tp2/personnes.py
@@ -9,8 +9,12 @@ class Habitant(ABC):
         self.adresse=adresse
         self.animaux=animaux
 
-    def affichage_adresse(self):
-        print("{nom} habite a {adresse}".format(nom=self.nom,adresse=self.adresse))
+    def __str__(self):
+        return "{} {}, {} ans,habite a {}".format(self.prenom,self.nom,self.age,self.adresse)
+
+def affichage(habitant):
+
+    print(str(habitant))
 
 class Adulte(Habitant):
     def __init__(self,nom,prenom,age,adresse):
@@ -40,7 +44,7 @@ class Enfant(Habitant):
     def calcul_nombre_annee_avant_retraite(self):
         if self.age >= 18:
             raise ValueError
-        else:
+        else:   
             return "Erreur: un enfant ne peut pas calculer sa retraite"
 
 # Adulte : leve une ValueError si age < 18
@@ -61,3 +65,16 @@ try:
     assert False, "une ValueError aurait du etre levee"
 except ValueError:
     pass
+
+
+adulte = Adulte("Dupont", "Marie", 35, "123 Rue de la Paix")
+enfant = Adulte("Mohamed", "ALGAZAR", 9, "123 VIVA L'ALGERIE")
+
+affichage(adulte)
+affichage(enfant)
+
+"""
+Rendre la méthode abstraite force toute nouvelle classe fille à l'implémenter explicitement sous peine de lever 
+une erreur dès l'instanciation, ce qui garantit qu'aucun objet Habitant ne pourra manquer de cette méthode lors
+ des futurs appels polymorphes
+"""
