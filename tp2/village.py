79935f561ed5658c10515e877ecc2b8f21e71ad5
Chhibi Anass <guillaumedelanoie01@gmail.com>
2026-09-22 15:21:43 +0200

    Exercice 4 fini

---
+++ tp2/village.py
@@ -0,0 +1,70 @@
+class Habitant:
+
+    def __init__ (self, nom, age, adresse, animaux=None):
+        self.__nom=nom
+        self.__age=age
+        self.__adresse=adresse
+        self.__animaux=animaux
+    @property
+    def get_nom(self):
+        return self.__nom
+    
+    @property
+    def set_nom(self,nom):
+        self.__nom=nom
+    
+    @property
+    def get_age(self):
+        return self.__age
+    
+    @property
+    def set_age(self,age):
+        self.__age=age
+    
+    @property
+    def get_adresse(self):
+        return self.__adresse
+    
+    @property
+    def set_adresse(self,adresse):
+        self.__adresse=adresse
+    
+    @property
+    def get_animaux(self):
+        return self.__animaux
+    
+    @property
+    def set_animaux(self,animaux):
+        self.__animaux=animaux
+
+
+    def affichage_adresse(self):
+        print("{nom} habite a {adresse}".format(nom=self.__nom,adresse=self.__adresse))
+
+    def compte_animal(self, animal):
+        if animal in self.__animaux :
+            return self.__animaux[animal]
+        else:
+            return 0
+
+    @property
+    def age(self):
+        return self.__age
+    @age.setter
+    def age(self,valeur):
+        if valeur > 130 or valeur < 0 :
+            raise ValueError
+        self.__age=valeur
+    
+
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
+
+h1.age = 26
+assert h1.age == 26
+try:
+    h1.age = -5
+    assert False, "une ValueError aurait du etre levee"
+except ValueError:
+    pass
+
